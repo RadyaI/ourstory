92813368b5c3e8e9d4a68fdde0acde15ca69a6da
RadyaI <radyaiftikhar@gmail.com>
2025-08-27 18:12:07 +0700

    fix quiz 1

--- app/quiz/1/page.tsx
+++ app/quiz/1/page.tsx
@@ -1,13 +1,17 @@
 "use client"
 
 import Image from "next/image"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import toast, { Toaster } from "react-hot-toast";
 
 export default function Q1() {
+    const [status, setStatus] = useState(true);
 
+    const [percobaan, setPercobaan] = useState(2)
     const [jawaban, setJawaban] = useState("B");
     const [userJawab, setUserJawab] = useState("")
+
+    const [timer, setTimer] = useState(60)
 
     function cekJawaban() {
         if (userJawab === "") {
@@ -18,7 +22,13 @@ export default function Q1() {
         if (jawaban === userJawab) {
             toast.success("Yeaayy benar 🎉")
         } else {
-            toast.error("Salaaah 😢 coba lagi!")
+            if (percobaan === 0) {
+                setStatus(false)
+                setTimer(60)
+                sessionStorage.setItem("status", "false")
+            }
+            setPercobaan((prev) => prev - 1)
+            toast.error(`Salaaah 😢, sisa ${percobaan} kali coba`)
         }
     }
 
@@ -30,36 +40,69 @@ export default function Q1() {
         { id: "E", text: "10" },
     ]
 
+    useEffect(() => {
+        setStatus(!sessionStorage.getItem("status"))
+    }, [])
+
+    useEffect(() => {
+        let interval: any;
+
+        if (!status && timer > 0) {
+            interval = setInterval(() => {
+                setTimer((prev) => prev - 1);
+            }, 1000);
+        }
+
+        if (timer === 0) {
+            setStatus(true);
+            setPercobaan(2);
+            sessionStorage.removeItem("status");
+        }
+
+        return () => clearInterval(interval);
+    }, [status, timer]);
+
     return (
         <>
             <Toaster position="top-right" />
             <div className="text-white mx-auto mt-5 w-[90%] md:w-[50%] h-[170px] md:h-[270px] relative">
-                <Image src={"/quiz/1.png"} alt="Soal 1" fill className="rounded-lg object-contain"/>
+                <Image src={"/quiz/1.png"} alt="Soal 1" fill className="rounded-lg object-contain" />
             </div>
 
-            {/* Pilihan Jawaban */}
             <div className="mb-20 text-white mx-auto mt-5 w-[90%] md:w-[50%] rounded-lg p-5 space-y-4 bg-gray-900/50">
                 {pilihan.map((item) => (
                     <button
                         key={item.id}
                         onClick={() => setUserJawab(item.id)}
-                        className={`w-full flex items-center gap-3 px-4 py-3 rounded-lg border 
-                            transition-all duration-200 
-                            ${userJawab === item.id 
-                                ? "bg-blue-600 border-blue-400 text-white shadow-lg scale-105" 
+                        disabled={!status}
+                        className={`w-full flex items-center gap-3 px-4 py-3 rounded-lg border transition-all duration-200 
+                            ${userJawab === item.id && status
+                                ? "bg-blue-600 border-blue-400 text-white shadow-lg scale-105"
                                 : "bg-gray-800 border-gray-700 hover:bg-gray-700"
-                            }`}
+                            } 
+                            ${!status ? "opacity-50 cursor-not-allowed" : ""}`}
                     >
                         <span className="font-bold">{item.id}.</span> {item.text}
                     </button>
                 ))}
 
-                <button 
-                    onClick={cekJawaban} 
-                    className="w-full mt-4 bg-green-600 hover:bg-green-700 px-4 py-3 rounded-lg font-semibold text-white transition-all"
-                >
-                    Submit Jawaban
-                </button>
+                {status && (
+                    <button
+                        onClick={cekJawaban}
+                        className="w-full mt-4 bg-green-600 hover:bg-green-700 px-4 py-3 rounded-lg font-semibold text-white transition-all"
+                    >
+                        Submit Jawaban
+                    </button>
+                )}
+
+                {!status && (
+                    <button
+                        onClick={() => toast.error("Tunggu yaaa ⏳")}
+                        className="w-full mt-4 cursor-not-allowed bg-red-600 px-4 py-3 rounded-lg font-semibold text-white transition-all"
+                    >
+                        STOP - batas percobaan habis - tunggu {timer} detik
+                    </button>
+                )}
             </div>
         </>
     )
